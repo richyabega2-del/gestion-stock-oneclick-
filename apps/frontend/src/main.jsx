9023854ec17574925c9711259d50858ee3273ccd
richy <richyabega2@gmail.com>
2026-07-11 00:41:33 +0100

    fix: configuration URL API backend pour axios

--- apps/frontend/src/main.jsx
+++ apps/frontend/src/main.jsx
@@ -1,3 +1,5 @@
+import axios from 'axios'
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
