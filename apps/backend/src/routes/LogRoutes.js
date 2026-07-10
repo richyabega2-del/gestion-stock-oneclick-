c25a85ca7c6bd0890b65e6a04c847be8ecebfc6b
richy <richyabega2@gmail.com>
2026-07-10 23:45:51 +0100

    fix: casse LogController

--- apps/backend/src/routes/LogRoutes.js
+++ apps/backend/src/routes/LogRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const logController = require('../controllers/logController')
+const logController = require('../controllers/LogController')
 const authMiddleware = require('../middlewares/authMiddleware')
 
 router.get('/',                        authMiddleware, logController.getLogs)
